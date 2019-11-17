9e7a96b65932c1e1a9c920ca558c6c80b1b9e81c
vineet.s <vineet.s@zohocorp.com>
2019-11-18 01:31:41 +0530

    customerReview

--- src/components/body.js
+++ src/components/body.js
@@ -5,6 +5,11 @@ import CategoryBox from './subComponents/categoryBox';
 import adventure from './../../public/images/categories/adventure.png';
 import laptop from './../../public/images/categories/laptop.png';
 import camera from './../../public/images/categories/camera.png';
+import { Typography, Divider } from '@material-ui/core';
+import ProductView from './subComponents/productView';
+import CustomerReview from './subComponents/customerReviews';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
 
 class Body extends React.Component {
 	render() {
@@ -15,6 +20,50 @@ class Body extends React.Component {
 					<CategoryBox icon={adventure} categoryName="Adventure" />
 					<CategoryBox icon={laptop} categoryName="Business" />
 					<CategoryBox icon={camera} categoryName="Cameras" />
+				</Grid>
+				<br />
+				<Typography variant="caption">
+					Now rent with us in <b>Bangalore</b>
+				</Typography>
+				<Grid container spacing={2} style={{ marginTop: '30px' }}>
+					<Grid item sm={4} xs={12}>
+						<Typography variant="h5" align="left" style={{ marginTop: '10px', marginLeft: '60px' }}>
+							Trending in
+						</Typography>
+						<Typography variant="h3" align="left" style={{ marginTop: '0px', marginLeft: '40px' }}>
+							Bangalore <TrendingUpIcon style={{ color: 'rgba(183, 28, 28,0.9)', fontSize: '40px' }} />
+						</Typography>
+					</Grid>
+					<Grid item xs={2}>
+						<ProductView />
+					</Grid>
+					<Grid item xs={2}>
+						<ProductView />
+					</Grid>
+					<Grid item xs={2}>
+						<ProductView />
+					</Grid>
+					<Grid item xs={2}>
+						<ProductView />
+					</Grid>
+				</Grid>
+
+				<Grid container spacing={2} style={{ marginTop: '30px' }}>
+					<Grid item sm={4} xs={12}>
+						<Typography variant="h5" align="left" style={{ marginTop: '10px', marginLeft: '60px' }}>
+							Reviews By
+						</Typography>
+						<Typography variant="h3" align="left" style={{ marginTop: '0px', marginLeft: '40px' }}>
+							Customers
+							<SentimentVerySatisfiedIcon style={{ color: 'rgba(183, 28, 28,0.9)', fontSize: '40px' }} />
+						</Typography>
+					</Grid>
+					<Grid item xs={4}>
+						<CustomerReview />
+					</Grid>
+					<Grid item xs={4}>
+						<CustomerReview />
+					</Grid>
 				</Grid>
 			</div>
 		);
---
+++ src/components/subComponents/customerReviews.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange, deepPurple } from '@material-ui/core/colors';
+import { Grid } from '@material-ui/core';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles({
+	card: {
+		maxWidth: 345
+	},
+	media: {
+		height: 140
+	},
+	avatar: {
+		margin: 10
+	}
+});
+
+export default function CustomerReview() {
+	const classes = useStyles();
+
+	return (
+		<Card className={classes.card}>
+			<CardActionArea>
+				<CardContent>
+					<Grid container justify="left" alignItems="center">
+						<Avatar className={classes.avatar}>L</Avatar>
+						<Typography gutterBottom variant="h5" component="h2">
+							Lucifer Morningstar
+						</Typography>
+					</Grid>
+					<Typography align="left" variant="body2" color="textSecondary" component="p">
+						Rentobuzz has one of the finest camera's. I really love the varities they provide.
+					</Typography>
+					<Box component="fieldset" mb={3} borderColor="transparent">
+						<Rating name="read-only" value="4" readOnly />
+					</Box>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
+}
