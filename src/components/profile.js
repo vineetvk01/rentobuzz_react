1db0792e7ba804bb06e71395a99bb712bcf4a58c
vineet.s <vineet.s@zohocorp.com>
2019-12-11 20:08:40 +0530

    ReactChanges

--- src/components/profile.js
+++ src/components/profile.js
@@ -8,6 +8,7 @@ import DatePicker from './microcomponents/datePicker';
 import ProductCard from './microcomponents/productsItem';
 import IconBreadcrumbs from './microcomponents/breadCrums';
 import ProfileMenu from './subComponents/profileMenu';
+import ProfileNotification from './microcomponents/profileNotification';
 
 class Profile extends React.Component {
 	constructor(props) {
@@ -25,7 +26,14 @@ class Profile extends React.Component {
 				<Grid item sm={3}>
 					<ProfileMenu />
 				</Grid>
-				<Grid item sm={9} />
+				<Grid item sm={9} style={{ padding: '5%' }}>
+					<ProfileNotification title="Active Order" message="You have 2 active orders." />
+					<ProfileNotification title="Last Logged In" message="You last logged in on 2nd June 2019" />
+					<ProfileNotification
+						title="Feedback Pending"
+						message="Your feedback is pending... Click here to get going.."
+					/>
+				</Grid>
 			</Grid>
 		);
 	}
